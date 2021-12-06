05e266074c17517d1514709ee18048aca077e42e
Jasmine-liang <mingchenliang954@gmail.com>
2021-12-06 15:42:03 +0800

    final project

--- app/src/App.js
+++ app/src/App.js
@@ -1,7 +1,7 @@
 import React, { useEffect, useState} from 'react';
 import './App.css';
 import twitterLogo from './assets/twitter-logo.svg';
-
+import CandyMachine from './CandyMachine';
 // Constants
 const TWITTER_HANDLE = '_buildspace';
 const TWITTER_LINK = `https://twitter.com/${TWITTER_HANDLE}`;
@@ -81,6 +81,8 @@ const [walletAddress, setWalletAddress] = useState(null);
           {/* Add the condition to show this only if we don't have a wallet address */}
           {!walletAddress && renderNotConnectedContainer()}
         </div>
+              {/* Check for walletAddress and then pass in walletAddress */}
+      {walletAddress && <CandyMachine walletAddress={window.solana} />}
         <div className="footer-container">
           <img alt="Twitter Logo" className="twitter-logo" src={twitterLogo} />
           <a
